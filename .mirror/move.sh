1740280e3c9c1d5f355030dcdcd576f776a4756e
mo2 <5617340+mo2@user.noreply.gitee.com>
2020-09-26 07:06:29 +0800

    TEST

--- .mirror/move.sh
+++ .mirror/move.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd  ~/gitee/zsh
-cp -rf update.sh zsh.sh tools .gitignore .termux .mirror ~/github/github-zsh/
+cp -rf zsh.sh tools .gitignore config .mirror ~/github/github-zsh/
 cd ~/github/github-zsh/.mirror 
 bash ./github.sh
 pwd
